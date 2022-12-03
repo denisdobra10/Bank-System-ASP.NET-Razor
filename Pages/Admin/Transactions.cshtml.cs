8d8c4d12736c066cd5e169aa2a21f8e4805b029f
Denis Dobra <82820448+denisdobra10@users.noreply.github.com>
2022-12-03 22:07:34 +0200

    Bank System fully working

---
+++ Pages/Admin/Transactions.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BankSystem.Data;
+using BankSystem.Models;
+
+namespace BankSystem.Pages.Admin
+{
+    public class TransactionsModel : PageModel
+    {
+        private readonly BankSystem.Data.BankSystemContext _context;
+
+        public TransactionsModel(BankSystem.Data.BankSystemContext context)
+        {
+            _context = context;
+        }
+
+      public Transaction Transaction { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Transaction == null)
+            {
+                return NotFound();
+            }
+
+            var transaction = await _context.Transaction.FirstOrDefaultAsync(m => m.Id == id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Transaction = transaction;
+            }
+            return Page();
+        }
+    }
+}
